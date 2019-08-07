6c86e976634886b8cca276847bef515f235b03d4
Justo Manrique <ja.manrique@pm.me>
2019-08-07 08:24:05 -0500

    pba

--- ieee-fraud-detection/ieee-fraud-detection.jl
+++ ieee-fraud-detection/ieee-fraud-detection.jl
@@ -1,10 +1,10 @@
 using DataFrames, Queryverse, CSV, StatsBase, Plots
 train_id=CSV.read("../../kaggle-data/train_identity.csv",copycols=true)
 train_trx=CSV.read("../../kaggle-data/train_transaction.csv",copycols=true)
-
 # Descripción de variables
 describe(train_trx, :all)
 train_trx |> @groupby(_.isFraud) |> @map({Key=key(_), count=length(_)}) |> DataFrame
-train_trx_fraud =train_trx |> @filter(_.isFraud==1) |> @select(:TransactionAmt)
-train_trx_fraud = convert(Vector,train_trx_fraud)
+train_trx_fraud =train_trx |> @filter(_.isFraud==1) |> @select(:TransactionAmt) |> DataFrame
+train_trx_fraud = convert(Array,train_trx_fraud)
 histogram(train_trx_fraud)
+train_trx |> @groupby(_.card1) |> @map({Key=key(_), count=length(_.isFraud)}) |> DataFrame
